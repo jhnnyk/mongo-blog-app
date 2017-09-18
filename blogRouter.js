0b77ec6907828c01f790df8b5faacce2001d770e
John Knotts <jhnnyk@gmail.com>
2017-09-18 13:52:54 -0600

    add PUT route for updating blog posts

--- blogRouter.js
+++ blogRouter.js
@@ -59,4 +59,27 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (`Request path id (${req.params.id}) and request body id (${req.body.id}) must match`)
+    console.error(message)
+    return res.status(400).json({message: message})
+  }
+
+  const toUpdate = {}
+  const updateableFields = ['title', 'content', 'author']
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field]
+    }
+  })
+
+  Post
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+    .then(post => res.status(200).json(post.apiRepr()))
+    .catch(err => res.status(500).json({message: 'Internal server error'}))
+})
+
 module.exports = router
