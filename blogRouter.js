0c77da4197c623e688ec346e214a0a1f0245deb1
John Knotts <jhnnyk@gmail.com>
2017-09-18 13:33:06 -0600

    add POST endpoint to add new blog post

--- blogRouter.js
+++ blogRouter.js
@@ -33,4 +33,30 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  const requiredFields = ['title', 'content', 'author']
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i]
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message)
+    }
+  }
+
+  Post
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(
+      post => res.status(201).json(post.apiRepr())
+    )
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({message: 'Internal server error'})
+    })
+})
+
 module.exports = router
